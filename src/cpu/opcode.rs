be081dee027aaeefdb976152a0ad05549c9aa140
Tibor Krizsak <tibork@topdesk.com>
2022-10-03 17:37:23 +0200

    Sorted operation functions based on opcodes

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -68,10 +68,22 @@ impl Opcode {
         self.current_operation = format!("{:02X} - ", current_opcode);
         self.table[current_opcode as usize](self, cpu)
     }
+    
+    pub fn brk_00(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("BRK");
+        let pc: [u8; 2] = self.u16_to_u8s(cpu.PC + 1);
+        cpu.push_on_stack(pc[0]);
+        cpu.push_on_stack(pc[1]);
+        cpu.push_on_stack(cpu.P);
+        cpu.PC = 0xFFFE;
+        cpu.set_flag(Flags::I, 1);
+        cpu.cycle += 7;
+    }
 
-    pub fn nop_ea(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("NOP");
-        cpu.cycle += 2;
+    pub fn php_08(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("PHP");
+        cpu.push_on_stack(cpu.P);
+        cpu.cycle += 3;
     }
 
     pub fn clc_18(&mut self, cpu : &mut MOS6510) {
@@ -80,22 +92,10 @@ impl Opcode {
         cpu.cycle += 2;
     }
 
-    pub fn cld_d8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("CLD");
-        cpu.set_flag(Flags::D, 0);
-        cpu.cycle += 2;
-    }
-
-    pub fn cli_58(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("CLI");
-        cpu.set_flag(Flags::I, 0);
-        cpu.cycle += 2;
-    }
-
-    pub fn clv_b8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("CLV");
-        cpu.set_flag(Flags::V, 0);
-        cpu.cycle += 2;
+    pub fn plp_28(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("PLP");
+        cpu.P = cpu.pull_from_stack();
+        cpu.cycle += 4;
     }
 
     pub fn sec_38(&mut self, cpu : &mut MOS6510) {
@@ -104,23 +104,39 @@ impl Opcode {
         cpu.cycle += 2;
     }
 
-    pub fn sed_f8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("SED");
-        cpu.set_flag(Flags::D, 1);
+    pub fn pha_48(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("PHA");
+        cpu.push_on_stack(cpu.A);
+        cpu.cycle += 3;
+    }
+
+    pub fn cli_58(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("CLI");
+        cpu.set_flag(Flags::I, 0);
         cpu.cycle += 2;
+    }
+
+    pub fn rts_60(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("RTS");
+        let stack: u16 = cpu.stack_addr();
+        let pc_low = cpu.mmu.read(stack);
+        let pc_high = cpu.mmu.read(stack - 1);
+        cpu.PC = self.u8s_to_u16(pc_high, pc_low) + 1;
+        cpu.S.wrapping_add(2);
+        cpu.cycle += 4;
+    }
+
+    pub fn pla_68(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("PLA");
+        cpu.A = cpu.pull_from_stack();
+        self.check_and_set_n(cpu.A, cpu);
+        self.check_and_set_z(cpu.A, cpu);
+        cpu.cycle += 4;
     }
 
     pub fn sei_78(&mut self, cpu : &mut MOS6510) {
         self.current_operation.push_str("SEI");
         cpu.set_flag(Flags::I, 1);
-        cpu.cycle += 2;
-    }
-
-    pub fn dex_ca(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("DEX");
-        cpu.X -= 1;
-        self.check_and_set_n(cpu.X, cpu);
-        self.check_and_set_z(cpu.X, cpu);
         cpu.cycle += 2;
     }
 
@@ -132,49 +148,17 @@ impl Opcode {
         cpu.cycle += 2;
     }
 
-    pub fn inx_e8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("INX");
-        cpu.X = cpu.X.wrapping_add(1);
-        self.check_and_set_n(cpu.X, cpu);
-        self.check_and_set_z(cpu.X, cpu);
-        cpu.cycle += 2;
-    }
-
-    pub fn iny_c8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("INY");
-        cpu.Y = cpu.Y.wrapping_add(1);
-        self.check_and_set_n(cpu.Y, cpu);
-        self.check_and_set_z(cpu.Y, cpu);
-        cpu.cycle += 2;
-    }
-
-    pub fn tax_aa(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("TAX");
-        cpu.X = cpu.A;
-        self.check_and_set_n(cpu.X, cpu);
-        self.check_and_set_z(cpu.X, cpu);
-        cpu.cycle += 2;
-    }
-
-    pub fn tay_a8(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("TAY");
-        cpu.Y = cpu.A;
-        self.check_and_set_n(cpu.Y, cpu);
-        self.check_and_set_z(cpu.Y, cpu);
-        cpu.cycle += 2;
-    }
-
-    pub fn tsx_ba(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("TSX");
-        cpu.X = cpu.S;
-        self.check_and_set_n(cpu.X, cpu);
-        self.check_and_set_z(cpu.X, cpu);
-        cpu.cycle += 2;
-    }
-
     pub fn txa_8a(&mut self, cpu : &mut MOS6510) {
         self.current_operation.push_str("TXA");
         cpu.A = cpu.X;
+        self.check_and_set_n(cpu.A, cpu);
+        self.check_and_set_z(cpu.A, cpu);
+        cpu.cycle += 2;
+    }
+
+    pub fn tya_98(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("TYA");
+        cpu.A = cpu.Y;
         self.check_and_set_n(cpu.A, cpu);
         self.check_and_set_z(cpu.A, cpu);
         cpu.cycle += 2;
@@ -188,60 +172,78 @@ impl Opcode {
         cpu.cycle += 2;
     }
 
-    pub fn tya_98(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("TYA");
-        cpu.A = cpu.Y;
-        self.check_and_set_n(cpu.A, cpu);
-        self.check_and_set_z(cpu.A, cpu);
+
+    pub fn tay_a8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("TAY");
+        cpu.Y = cpu.A;
+        self.check_and_set_n(cpu.Y, cpu);
+        self.check_and_set_z(cpu.Y, cpu);
         cpu.cycle += 2;
     }
 
-    pub fn pha_48(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("PHA");
-        cpu.push_on_stack(cpu.A);
-        cpu.cycle += 3;
+    pub fn tax_aa(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("TAX");
+        cpu.X = cpu.A;
+        self.check_and_set_n(cpu.X, cpu);
+        self.check_and_set_z(cpu.X, cpu);
+        cpu.cycle += 2;
     }
 
-    pub fn php_08(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("PHP");
-        cpu.push_on_stack(cpu.P);
-        cpu.cycle += 3;
+    pub fn clv_b8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("CLV");
+        cpu.set_flag(Flags::V, 0);
+        cpu.cycle += 2;
     }
 
-    pub fn pla_68(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("PLA");
-        cpu.A = cpu.pull_from_stack();
-        self.check_and_set_n(cpu.A, cpu);
-        self.check_and_set_z(cpu.A, cpu);
-        cpu.cycle += 4;
+    pub fn tsx_ba(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("TSX");
+        cpu.X = cpu.S;
+        self.check_and_set_n(cpu.X, cpu);
+        self.check_and_set_z(cpu.X, cpu);
+        cpu.cycle += 2;
     }
 
-    pub fn plp_28(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("PLP");
-        cpu.P = cpu.pull_from_stack();
-        cpu.cycle += 4;
+    pub fn iny_c8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("INY");
+        cpu.Y = cpu.Y.wrapping_add(1);
+        self.check_and_set_n(cpu.Y, cpu);
+        self.check_and_set_z(cpu.Y, cpu);
+        cpu.cycle += 2;
     }
 
-    pub fn rts_60(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("RTS");
-        let stack: u16 = cpu.stack_addr();
-        let pc_low = cpu.mmu.read(stack);
-        let pc_high = cpu.mmu.read(stack - 1);
-        cpu.PC = self.u8s_to_u16(pc_high, pc_low) + 1;
-        cpu.S.wrapping_add(2);
-        cpu.cycle += 4;
+    pub fn dex_ca(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("DEX");
+        cpu.X -= 1;
+        self.check_and_set_n(cpu.X, cpu);
+        self.check_and_set_z(cpu.X, cpu);
+        cpu.cycle += 2;
     }
 
-    pub fn brk_00(&mut self, cpu : &mut MOS6510) {
-        self.current_operation.push_str("BRK");
-        let pc: [u8; 2] = self.u16_to_u8s(cpu.PC + 1);
-        cpu.push_on_stack(pc[0]);
-        cpu.push_on_stack(pc[1]);
-        cpu.push_on_stack(cpu.P);
-        cpu.PC = 0xFFFE;
-        cpu.set_flag(Flags::I, 1);
-        cpu.cycle += 7;
+    pub fn cld_d8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("CLD");
+        cpu.set_flag(Flags::D, 0);
+        cpu.cycle += 2;
     }
+
+    pub fn inx_e8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("INX");
+        cpu.X = cpu.X.wrapping_add(1);
+        self.check_and_set_n(cpu.X, cpu);
+        self.check_and_set_z(cpu.X, cpu);
+        cpu.cycle += 2;
+    }
+
+    pub fn nop_ea(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("NOP");
+        cpu.cycle += 2;
+    }
+
+    pub fn sed_f8(&mut self, cpu : &mut MOS6510) {
+        self.current_operation.push_str("SED");
+        cpu.set_flag(Flags::D, 1);
+        cpu.cycle += 2;
+    }
+    
 
     // --- HELPER FUNCTIONS ---
 
