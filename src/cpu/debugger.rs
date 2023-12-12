26048472a03e6cc1792aae587d146d0048ea67b3
krtibo <tibor.krizsak@gmail.com>
2023-12-12 21:14:24 +0100

    Clean up debugger

--- src/cpu/debugger.rs
+++ src/cpu/debugger.rs
@@ -3,6 +3,7 @@
 
 extern crate sfml;
 use sfml::graphics::*;
+use sfml::system::Vector2f;
 use std::borrow::Borrow;
 use sfml::SfBox;
 use sfml::window::*;
@@ -10,23 +11,13 @@ use sfml::window::mouse::*;
 use super::cpu::MOS6510;
 use std::collections::VecDeque;
 use sfml::system::Vector2i;
+use sfml::window::Event::*;
 
 pub struct EventCodes {
     pub OK     : u8,
     pub EXIT   : u8,
     pub PAUSE  : u8,
     pub RESET  : u8,
-}
-
-impl EventCodes {
-    pub fn new() -> EventCodes {
-        EventCodes {
-            OK      : 0,
-            EXIT    : 1,
-            PAUSE   : 2,
-            RESET   : 3,
-        }
-    }
 }
 
 pub struct Snapshot {
@@ -40,13 +31,14 @@ pub struct Snapshot {
 
 impl Snapshot {
     pub fn new() -> Snapshot {
+        let empty = "".to_string();
         Snapshot {
-            instruction : "".to_string(),
-            AX          : "".to_string(),
-            SY          : "".to_string(),
-            PC          : "".to_string(),
-            Pb          : "".to_string(),
-            cycle       : "".to_string(),
+            instruction : empty.clone(),
+            AX          : empty.clone(),
+            SY          : empty.clone(),
+            PC          : empty.clone(),
+            Pb          : empty.clone(),
+            cycle       : empty.clone(),
         }
     }
 }
@@ -87,7 +79,7 @@ impl Debugger {
             LIGHT_RED       : Color::rgb(255, 119, 119),
             BLACK           : Color::rgb(51, 51, 51),
             snapshots       : VecDeque::new(),
-            events          : EventCodes::new(),
+            events          : EventCodes { OK: 0, EXIT: 1, PAUSE: 2, RESET: 3 },
             is_paused       : false,
         }
     }
@@ -103,71 +95,92 @@ impl Debugger {
         self.dbg.display();
     }
 
+    fn poll_keyboard(&mut self, event: Event) -> u8 {
+        match event {
+            KeyPressed { code, .. } => match code {
+                Key::Escape => { 
+                    self.dbg.close(); 
+                    return self.events.EXIT; },
+                Key::Space => { 
+                    self.is_paused = !self.is_paused; 
+                    return self.events.PAUSE; },
+                Key::R => { 
+                    if (!self.is_paused) { self.clear_snapshots(); }
+                    return self.events.RESET; },
+                Key::Down => { 
+                    if (self.is_paused) {
+                        if (self.line_count - 1 > self.active_state) {
+                            self.active_state += 1;
+                            if (self.active_state > 48) {
+                                self.selected_state = 48;
+                            } else {
+                                self.selected_state = self.active_state;
+                            }
+                        }
+                    } 
+                    return self.events.OK;
+                },
+                Key::Up => {
+                    if (self.is_paused) {
+                        if (self.active_state > 0) {
+                            self.active_state -= 1;
+                            if (self.active_state > 48) {
+                                self.selected_state = 48;
+                            } else {
+                                self.selected_state = self.active_state;
+                            }
+                        }
+                    }
+                    self.events.OK
+                },
+                _ => self.events.OK
+            },
+            _ => self.events.OK
+        }
+    }
+
+    fn poll_mouse(&mut self, event: Event) -> u8 {
+        match event {
+            MouseWheelScrolled { wheel, delta, .. } => match wheel {
+                Wheel::VerticalWheel => {
+                    if (delta > 0.0 && self.is_paused) {
+                        if (self.line_count - 1 > self.active_state) {
+                            self.active_state += 1;
+                            if (self.active_state > 48) {
+                                self.selected_state = 48;
+                            } else {
+                                self.selected_state = self.active_state;
+                            }
+                        }
+                    }
+                    if (delta < 0.0 && self.is_paused) {
+                        if (self.active_state > 0) {
+                            self.active_state -= 1;
+                            if (self.active_state > 48) {
+                                self.selected_state = 48;
+                            } else {
+                                self.selected_state = self.active_state;
+                            }
+                        }
+                    }
+                    self.events.OK
+                },
+                _ => self.events.OK
+            }
+            _ => self.events.OK
+        }
+    }
+
     pub fn poll(&mut self) -> u8 {
         while let Some(event) = self.dbg.poll_event() {
-            use crate::Event::*;
+            let keyboard_event = self.poll_keyboard(event);
+            if keyboard_event != 0 { return keyboard_event; }
+
+            let mouse_event = self.poll_mouse(event);
+            if mouse_event != 0 { return mouse_event; }
+
             match event {
                 Closed => { self.dbg.close(); return self.events.EXIT; }
-                KeyPressed { code, .. } => match code {
-                    Key::Escape => { 
-                        self.dbg.close(); 
-                        return self.events.EXIT; },
-                    Key::Space => { 
-                        self.is_paused = !self.is_paused; 
-                        return self.events.PAUSE; },
-                    Key::R => { 
-                        if (!self.is_paused) { self.clear_snapshots(); }
-                        return self.events.RESET; },
-                    Key::Down => { 
-                        if (self.is_paused) {
-                            if (self.line_count - 1 > self.active_state) {
-                                self.active_state += 1;
-                                if (self.active_state > 48) {
-                                    self.selected_state = 48;
-                                } else {
-                                    self.selected_state = self.active_state;
-                                }
-                            }
-                        } },
-                    Key::Up => {
-                        if (self.is_paused) {
-                            if (self.active_state > 0) {
-                                self.active_state -= 1;
-                                if (self.active_state > 48) {
-                                    self.selected_state = 48;
-                                } else {
-                                    self.selected_state = self.active_state;
-                                }
-                            }
-                        }
-                    },
-                    _ => {}
-                },
-                MouseWheelScrolled { wheel, delta, .. } => match wheel {
-                    Wheel::VerticalWheel => {
-                        if (delta > 0.0 && self.is_paused) {
-                            if (self.line_count - 1 > self.active_state) {
-                                self.active_state += 1;
-                                if (self.active_state > 48) {
-                                    self.selected_state = 48;
-                                } else {
-                                    self.selected_state = self.active_state;
-                                }
-                            }
-                        }
-                        if (delta < 0.0 && self.is_paused) {
-                            if (self.active_state > 0) {
-                                self.active_state -= 1;
-                                if (self.active_state > 48) {
-                                    self.selected_state = 48;
-                                } else {
-                                    self.selected_state = self.active_state;
-                                }
-                            }
-                        }
-                    },
-                    _ => {}
-                }
                 _ => {}
             }
         }
@@ -175,7 +188,6 @@ impl Debugger {
     }
 
     pub fn create_snapshot(&mut self, text: String, cpu: &MOS6510) {
-
         if self.snapshots.len() == 255 {
             self.snapshots.pop_back();
         }
@@ -188,7 +200,6 @@ impl Debugger {
         snapshot.Pb = format!("{:08b}", cpu.P);
         snapshot.cycle = format!("CYCLE:   {}", cpu.cycle);
         self.snapshots.push_front(snapshot);
-
     }
 
     pub fn clear_snapshots(&mut self) {
@@ -211,28 +222,11 @@ impl Debugger {
             texture.update_from_pixels(&texture_pixels, 1, 1, 0, 0);
         }
 
-        // separator sprite based on the generic texture with scaling
-        let mut separator_sprite = Sprite::with_texture(&texture);
-        separator_sprite.set_position((1011.0, 0.0));
-        separator_sprite.set_scale((4.0, 1600.0));
-
-        // text background sprite based on the generic texture with scaling
-        let mut text_background_sprite = Sprite::with_texture(&texture);
-        text_background_sprite.set_scale((BG_WIDTH as f32, BG_HEIGHT as f32));
-
-        // header background sprite based on the generic texture with scaling
-        // and rendering
-        let mut header_background_sprite = Sprite::with_texture(&texture);
-        header_background_sprite.set_position((0.0, 0.0));
-        header_background_sprite.set_scale((1850.0, BG_HEIGHT as f32));
-        self.dbg.draw(&header_background_sprite);
+        // header background sprite based on the generic texture with scaling and rendering
+        self.render_sprite(&texture, Vector2f::new(0.0, 0.0), Vector2f::new(1850.0, BG_HEIGHT as f32));
 
         // header text initialization and rendering
-        let mut render_text_header = Text::new("    PC    |           CPU INSTRUCTION           |      REGISTERS & FLAGS",
-                                               &self.font, 22);
-        render_text_header.set_position((15.0, 3.0));
-        render_text_header.set_fill_color(self.DARK_BLUE);
-        self.dbg.draw(&render_text_header);
+        self.render_text("    PC    |           CPU INSTRUCTION           |      REGISTERS & FLAGS".to_string(), 15.0, 3.0, self.DARK_BLUE);
 
         let mut start_index = 0;
         let mut end_index = 0;
@@ -250,72 +244,62 @@ impl Debugger {
 
         let mut line_number = 0;
         for i in start_index..end_index as u8 {
-            let mut render_text = Text::new(&self.snapshots[i as usize].instruction, &self.font, 22);
-            render_text.set_position((15.0, line_number as f32 * 32.0 + 37.0));
-
             if (line_number == self.selected_state && self.is_paused) {
                 let mut bg_sprite = self.selected_state as usize * 32 + 34;
                 if (bg_sprite > 1570) {
                     bg_sprite = 1570;
                 }
-                text_background_sprite.set_position((0.0, bg_sprite as f32));
-                self.dbg.draw(&text_background_sprite);
-                render_text.set_fill_color(self.DARK_BLUE);
+                // text background sprite based on the generic texture with scaling
+                self.render_sprite(&texture, Vector2f::new(0.0, bg_sprite as f32), Vector2f::new(BG_WIDTH as f32, BG_HEIGHT as f32));
+                self.render_text(self.snapshots[i as usize].instruction.clone(), 15.0, line_number as f32 * 32.0 + 37.0, self.DARK_BLUE);
             } else {
-                render_text.set_fill_color(self.LIGHT_BLUE);
+                self.render_text(self.snapshots[i as usize].instruction.clone(), 15.0, line_number as f32 * 32.0 + 37.0, self.LIGHT_BLUE);
             }
             if (self.snapshots[i as usize].instruction.contains("PPU RENDER")) {
-                render_text.set_fill_color(self.PURPLE);
+                self.render_text(self.snapshots[i as usize].instruction.clone(), 15.0, line_number as f32 * 32.0 + 37.0, self.PURPLE);
             }
             line_number += 1;
-            self.dbg.draw(&render_text);
         }
-        self.dbg.draw(&separator_sprite);
-    } // render_instructions
+        // separator sprite based on the generic texture with scaling
+        self.render_sprite(&texture, Vector2f::new(1011.0, 0.0), Vector2f::new(4.0, 1600.0));
+    }
+
+    fn render_text(&mut self, value: String, horizontal_position: f32, vertical_position: f32, color: Color) {
+        let font_size = 22;
+        let mut render_register = Text::new(&value, &self.font, font_size);
+        render_register.set_position((horizontal_position, vertical_position));
+        render_register.set_fill_color(color);
+        self.dbg.draw(&render_register);
+    }
+    
+    fn render_sprite(&mut self, texture: &Texture, position: Vector2f, scale: Vector2f) {
+        let mut sprite = Sprite::with_texture(texture);
+        sprite.set_position(position);
+        sprite.set_scale(scale);
+        self.dbg.draw(&sprite);
+    }
 
     pub fn render_registers(&mut self) {
         // takes the register states from the snapshots and renders them
-        let mut render_text_registers = Text::new(&self.snapshots[self.active_state as usize].AX, &self.font, 22);
-        render_text_registers.set_position((1032.0, 38.0));
-        render_text_registers.set_fill_color(self.LIGHT_RED);
-        self.dbg.draw(&render_text_registers);
-
-        render_text_registers = Text::new(&self.snapshots[self.active_state as usize].SY, &self.font, 22);
-        render_text_registers.set_position((1032.0, 70.0));
-        render_text_registers.set_fill_color(self.LIGHT_RED);
-        self.dbg.draw(&render_text_registers);
-
-        render_text_registers = Text::new(&self.snapshots[self.active_state as usize].PC, &self.font, 22);
-        render_text_registers.set_position((1032.0, 102.0));
-        render_text_registers.set_fill_color(self.LIGHT_RED);
-        self.dbg.draw(&render_text_registers);
-
-        render_text_registers = Text::new("FLAGS:", &self.font, 22);
-        render_text_registers.set_position((1032.0, 166.0));
-        render_text_registers.set_fill_color(self.LIGHT_RED);
-        self.dbg.draw(&render_text_registers);
+        self.render_text(self.snapshots[self.active_state as usize].AX.clone(), 1032.0, 38.0, self.LIGHT_RED);
+        self.render_text(self.snapshots[self.active_state as usize].SY.clone(), 1032.0, 70.0, self.LIGHT_RED);
+        self.render_text(self.snapshots[self.active_state as usize].PC.clone(), 1032.0, 102.0, self.LIGHT_RED);
+        self.render_text("FLAGS:".to_string(), 1032.0, 166.0, self.LIGHT_RED);
 
         // flags text and coloring based on turned on or off
         let flags_label: String = "NV1BDIZC".to_string();
         for i in 0..self.snapshots[self.active_state as usize].Pb.len() {
-            render_text_registers = Text::new(&flags_label[i..i+1], &self.font, 22);
-            render_text_registers.set_position((1200.0 + i as f32 * 40.0, 166.0));
             if self.snapshots[self.active_state as usize].Pb.chars().nth(i).unwrap() == '1' {
-                render_text_registers.set_fill_color(self.LIGHT_RED);
+                self.render_text(flags_label[i..i+1].to_string().clone(), 1200.0 + i as f32 * 40.0, 166.0, self.LIGHT_RED);
             } else {
-                render_text_registers.set_fill_color(self.LIGHT_BLUE);
+                self.render_text(flags_label[i..i+1].to_string().clone(), 1200.0 + i as f32 * 40.0, 166.0, self.LIGHT_BLUE);
             }
-            self.dbg.draw(&render_text_registers);
         }
 
-        render_text_registers = Text::new(&self.snapshots[self.active_state as usize].cycle, &self.font, 22);
-        render_text_registers.set_position((1032.0, 198.0));
-        render_text_registers.set_fill_color(self.LIGHT_RED);
-        self.dbg.draw(&render_text_registers);
-    } // render_registers
+        self.render_text(self.snapshots[self.active_state as usize].cycle.clone(), 1032.0, 198.0, self.LIGHT_RED);
+    }
 
     pub fn memory_map(&mut self, ram: &[u8]) {
-
         let mut pixels: [u8; 256 * 256 * 4] = [255; 256 * 256 * 4];
         let current_pc_str = &self.snapshots[self.active_state as usize].PC[6..];
         let current_pc: usize = usize::from_str_radix(current_pc_str, 16).unwrap();
