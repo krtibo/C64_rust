79922ac98486ac19ff0ffbfc672f9bda4e96d9b7
krtibo <tibor.krizsak@gmail.com>
2023-12-23 12:53:38 +0100

    tests for adc, cmp instructions

--- src/cpu/instructions/test/mod.rs
+++ src/cpu/instructions/test/mod.rs
@@ -1,1 +1,17 @@
 pub mod load_test;
+pub mod arith_test;
+
+use crate::MOS6510;
+use super::super::Opcode;
+
+pub fn setup() -> TestSetup {
+    let mut cpu: MOS6510 = MOS6510::new(); 
+    cpu.PC = 0x0000;
+    let mut opc: Opcode = Opcode::new();
+    TestSetup { cpu, opc }
+}
+
+pub struct TestSetup {
+    pub cpu: MOS6510,
+    pub opc: Opcode,
+}
