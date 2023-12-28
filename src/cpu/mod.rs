33e327a9e7c6c92d9c8257912414633d963938e6
krtibo <tibor.krizsak@gmail.com>
2023-12-28 15:29:51 +0100

    add tests for flag setter/getter utility functions

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,9 +3,11 @@ pub mod mmu;
 pub mod debugger;
 pub mod instructions;
 pub mod ppu;
+pub mod test;
 
 pub use self::cpu::MOS6510;
 pub use self::cpu::Flags;
+pub use cpu::*;
 pub use self::mmu::MMU;
 pub use self::debugger::debugger::Debugger;
 pub use self::instructions::opcode::Opcode;
